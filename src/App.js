89c243e49651c8f89afd9744daba857dbe596678
Traorasp <miguelsolanopaz@outlook.com>
2022-06-22 13:55:30 -0400

    Added logic to change score and high score

--- src/App.js
+++ src/App.js
@@ -1,14 +1,40 @@
+import React, { useEffect, useState } from 'react';
 import Cards from './components/Cards';
 
 function App() {
+  const [score, setScore] = useState(0);
+  const [highScore, setHighScore] = useState(0);
+
+  const increaseScore = () => {
+    setScore(score + 1);
+  };
+
+  const lostGame = () => {
+    setScore(0);
+  };
+
+  useEffect(() => {
+    if (score > highScore) {
+      setHighScore(score);
+    }
+  }, [score]);
+
   return (
     <div>
       <h1>Memory Card</h1>
       <div>
-        <h2>High Score:</h2>
-        <h2>Score:</h2>
+        <h2>
+          High Score:
+          {' '}
+          {highScore}
+        </h2>
+        <h2>
+          Score:
+          {' '}
+          {score}
+        </h2>
       </div>
-      <Cards />
+      <Cards onCardClick={increaseScore} onFail={lostGame} />
     </div>
   );
 }
